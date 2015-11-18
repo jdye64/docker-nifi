c785f8d0778c9669a316ce02dcc91b0e98da6fab
Andrew Grande <aperepel@gmail.com>
2015-11-17 20:25:31 -0500

    Match hostnames everywhere

--- docker/run.sh
+++ docker/run.sh
@@ -25,17 +25,17 @@ configure_site2site() {
 }
 
 configure_cluster_node() {
+  sed -i "s/nifi\.web\.http\.host=/nifi.web.http.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.address=/nifi.cluster.node.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.protocol\.port=/nifi.cluster.node.protocol.port=12346/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=/nifi.cluster.node.unicast.manager.address=ncm/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=/nifi.cluster.node.unicast.manager.address=cluster-ncm/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=/nifi.cluster.node.unicast.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
 }
 
 configure_cluster_manager() {
   sed -i "s/nifi\.cluster\.is\.manager=false/nifi.cluster.is.manager=true/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.is\.manager=false/nifi.cluster.is.manager=true/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.manager\.address=/nifi.cluster.manager.address=ncm/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.manager\.address=/nifi.cluster.manager.address=cluster-ncm/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.manager\.protocol\.port=/nifi.cluster.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
 }
 
